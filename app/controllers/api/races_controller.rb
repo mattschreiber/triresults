a349b96e00d5946f11fbf840ca59cb687e5060d8
mattschreiber <matt.schreiber01@gmail.com>
2016-07-06 11:10:16 -0400

    POST /api/races creates a new Race in database and satisfies requirements

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -20,11 +20,23 @@ module Api
 		def create
 			if !request.accept || request.accept == "*/*"
 				# render plain: :nothing, status: :ok
-				render plain: "#{params[:race][:name]}"
+				render plain: "#{params[:race][:name]}", status: :ok
 			else
-				#real implementation
+				@race = Race.create(race_params)
+				render plain: "#{params[:race][:name]}", status: :created
 			end
 		end
+
+		private
+    	# Use callbacks to share common setup or constraints between actions.
+    	def set_race
+      	@race = Race.find(params[:id])
+    	end
+
+    	# Never trust parameters from the scary internet, only allow the white list through.
+    	def race_params
+      	params.require(:race).permit(:name, :date, :city, :state, :swim_distance, :swim_units, :bike_distance, :bike_units, :run_distance, :run_units)
+    	end
 	end
 
 end
