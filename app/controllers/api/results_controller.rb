45f6e10f862d51afb9410a3e1c7335f1dbcb4e97
mattschreiber <matt.schreiber01@gmail.com>
2016-07-06 16:48:23 -0400

    GET /api/races/:race_id/results/:id returns a specific entrant for a race

--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -7,6 +7,8 @@ module Api
 				render plain: "/api/races/#{params[:race_id]}/results"
 			else
 				#real implementation ...
+				@result=Race.find(params[:race_id]).entrants.where(:id=>params[:id]).first
+				render :partial=>"result", :object=>@result
 			end
 		end
 
@@ -15,7 +17,7 @@ module Api
 			if !request.accept || request.accept == "*/*"
 				render plain: "/api/races/#{params[:race_id]}/results/#{params[:id]}"
 			else
-				#real implementation ...
+				@result=Race.find(params[:race_id]).entrants.where(:id=>params[:id]).first
 			end
 		end
 	end
